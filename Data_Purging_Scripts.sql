61ea11427030a2110763bb45b71c165db2690c75
Ravin D <ravin.d3107@outlook.com>
2023-09-21 11:12:48 +0530

    This is for DB Creation and Dropping. It also has procedure to copy a table from one DB to another DB which includes schemas as well

---
+++ Data_Purging_Scripts.sql
@@ -0,0 +1,56 @@
+-- Creating a Database
+
+Create procedure CreateDB
+@db_name nvarchar(20)
+as
+begin
+
+	declare @sql_query nvarchar(MAX)
+	set @sql_query = ('Create Database '+ @db_name)
+	exec(@sql_query)
+end;
+
+
+-- Dropping a Database
+
+Create procedure DropDB
+@db_name nvarchar(20)
+as
+begin
+	
+	declare @sql_query nvarchar(MAX)
+	if exists (select * from sys.databases where name = @db_name)
+    begin
+        set @sql_query = ('Drop Database '+@db_name)
+        exec (@sql_query)
+    end
+    else
+    begin
+        print 'Database does not exist.'
+    end
+end;
+
+
+
+-- Create Table and Insert Data from Source DB to Destination DB
+
+CREATE PROCEDURE CopyTableFromSourceToTarget2
+    @SourceDatabase NVARCHAR(100),
+	@SourceSchema nvarchar(100),
+    @SourceTable NVARCHAR(100),
+    @TargetDatabase NVARCHAR(100),
+	@TargetSchema nvarchar(100),
+    @TargetTable NVARCHAR(100)
+AS
+BEGIN
+    DECLARE @SQL NVARCHAR(MAX);
+
+    SET @SQL = '
+        USE ' + QUOTENAME(@TargetDatabase) + ';
+        SELECT *
+        INTO ' + QUOTENAME(@TargetDatabase) + '.'+ QUOTENAME(@TargetSchema) +'.' + QUOTENAME(@TargetTable) + '
+        FROM ' + QUOTENAME(@SourceDatabase) + '.'+ QUOTENAME(@SourceSchema) +'.' + QUOTENAME(@SourceTable) + ';
+    ';
+
+    EXEC(@SQL);
+END;
