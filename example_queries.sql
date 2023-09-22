3a6d76e43c92b79d78de8b3a83b1578cb9334b8d
Ravin D <ravin.d3107@outlook.com>
2023-09-22 14:26:05 +0530

    Added some guidelines in example_queries.sql for better functioning of the procedure named COPY_ALL_DATA_INCLUDING_CHILD_TABLES_Modified

--- example_queries.sql
+++ example_queries.sql
@@ -3,3 +3,7 @@ exec DropDB 'sampledb';
 
 exec CopyTableFromSourceToTarget2 'AdventureWorks2022', 'HumanResources','Department', 'sampledb', 'dbo','sampledb_table3';
 exec CopyTableFromSourceToTargetWithConditions 'AdventureWorks2022', 'HumanResources','Department', 'sampledb', 'dbo','sampledb_table5','where GroupName = ''Sales and Marketing''';
+
+-- For using COPY_ALL_DATA_INCLUDING_CHILD_TABLES_Modified, you have to be in the source database itself for proper functioning
+use AdventureWorks2022;
+exec COPY_ALL_DATA_INCLUDING_CHILD_TABLES_Modified 'AdventureWorks2022', 'Person', 'Person', 'AdventureWorks2022Backup', 'dbo', 'Person', '';
