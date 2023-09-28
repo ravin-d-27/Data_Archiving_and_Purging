3f2cecab2a07ebad2ec851e6bf9c4da610bc7df7
Ravin D <ravin.d3107@outlook.com>
2023-09-28 12:12:13 +0530

    Added a Procedure with creates Logs and Log Table while transferring one database to another database

--- Data_Purging_Scripts_without_schemas.sql
+++ Data_Purging_Scripts_without_schemas.sql
@@ -111,3 +111,86 @@ BEGIN
     -- Clean up temporary table
     DROP TABLE #ChildTables;
 END;
+
+
+
+CREATE PROCEDURE COPY_ALL_DATA_WITH_LOGS
+    @SourceDatabase NVARCHAR(100),
+    @SourceSchema NVARCHAR(100),
+    @SourceTable NVARCHAR(100),
+    @TargetDatabase NVARCHAR(100),
+    @TargetSchema NVARCHAR(100),
+    @TargetTable NVARCHAR(100),
+    @condition NVARCHAR(MAX),
+    @LogsID INT OUTPUT
+AS
+BEGIN
+    DECLARE @SQL NVARCHAR(MAX);
+    DECLARE @ChildTableName NVARCHAR(100);
+
+    -- Create temporary table to store child table names
+    CREATE TABLE #ChildTables (TableName NVARCHAR(100));
+
+    -- Find child tables
+    INSERT INTO #ChildTables
+    SELECT t.name
+    FROM sys.foreign_keys AS fk
+    INNER JOIN sys.tables AS t ON fk.parent_object_id = t.object_id
+    WHERE OBJECT_NAME(fk.referenced_object_id) = @SourceTable
+      AND SCHEMA_NAME(t.schema_id) = @SourceSchema
+      AND DB_NAME() = @SourceDatabase;
+
+    -- Create Log_Table if it doesn't exist
+	SET @SQL = '
+		CREATE TABLE ' + QUOTENAME(@TargetDatabase) + '.' + QUOTENAME(@TargetSchema) + '.Log_Table (
+			LogsID INT IDENTITY(1,1) PRIMARY KEY,
+			LogMessage NVARCHAR(MAX),
+			CreationDateTime DATETIME DEFAULT GETDATE()
+		);
+	';
+	EXEC(@SQL);
+
+    -- Insert a record into Log_Table and get the generated LogsID
+    SET @SQL = '
+        USE ' + QUOTENAME(@TargetDatabase) + ';
+        INSERT INTO ' + QUOTENAME(@TargetDatabase) +'.'+QUOTENAME(@TargetSchema) + '.Log_Table (LogMessage)
+        VALUES (''Log message for this run'');
+        SET @LogsID = SCOPE_IDENTITY();';
+    EXEC sp_executesql @SQL, N'@LogsID INT OUTPUT', @LogsID OUTPUT;
+
+    -- Copy data from source table to target table
+    SET @SQL = '
+        USE ' + QUOTENAME(@TargetDatabase) + ';
+        SELECT *, @LogsID AS LogsID
+        INTO ' + QUOTENAME(@TargetDatabase) + '.' + QUOTENAME(@TargetSchema) + '.' + QUOTENAME(@TargetTable) + '
+        FROM ' + QUOTENAME(@SourceDatabase) + '.' + QUOTENAME(@SourceSchema) + '.' + QUOTENAME(@SourceTable) +
+		@condition + ';';
+    EXEC sp_executesql @SQL, N'@LogsID INT', @LogsID;
+
+    -- Create child tables and constraints
+    DECLARE childTablesCursor CURSOR FOR
+    SELECT TableName FROM #ChildTables;
+
+    OPEN childTablesCursor;
+    FETCH NEXT FROM childTablesCursor INTO @ChildTableName;
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        -- Generate SQL to create child table in target database
+        SET @SQL = '
+            USE ' + QUOTENAME(@TargetDatabase) + ';
+            SELECT *, @LogsID AS LogsID
+            INTO ' + QUOTENAME(@TargetSchema) + '.' + QUOTENAME(@ChildTableName) + '
+            FROM ' + QUOTENAME(@SourceDatabase) + '.' + QUOTENAME(@SourceSchema) + '.' + QUOTENAME(@ChildTableName) + 
+            @condition + ';';
+        EXEC sp_executesql @SQL, N'@LogsID INT', @LogsID;
+
+        FETCH NEXT FROM childTablesCursor INTO @ChildTableName;
+    END;
+
+    CLOSE childTablesCursor;
+    DEALLOCATE childTablesCursor;
+
+    -- Clean up temporary table
+    DROP TABLE #ChildTables;
+END;
