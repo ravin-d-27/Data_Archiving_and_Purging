58b545fa5aa947535f8c1bfa51434cbf1bc08264
Ravin D <ravin.d3107@outlook.com>
2023-10-20 11:14:20 +0530

    Final Changes

--- Data_Purging_Updated.sql
+++ Data_Purging_Updated.sql
@@ -409,3 +409,264 @@ END;
 -- Executing Command
 
 exec UPDATION_OF_DATA_FINAL_17_TEST'TestDB', 'dbo', 'ParentTable', 'TestDB_Backup', 'dbo', 'ParentTable', '', '','Copying';
+
+
+
+-- Final Working procedure for Updating the data even though columns are extra or less in source table using Dynamic SQL
+
+CREATE PROCEDURE Find_Extra_Columns_Modified_2
+    @source_table_name NVARCHAR(255),
+    @destination_table_name NVARCHAR(255),
+	@ColumnList NVARCHAR(MAX) OUTPUT
+AS
+BEGIN
+    -- Create temporary tables to store column names
+    CREATE TABLE #temp_columns (column_name NVARCHAR(255));
+	CREATE TABLE #temp_columns2 (column_name NVARCHAR(255));
+    
+    DECLARE @source_db NVARCHAR(255);
+    SET @source_db = PARSENAME(@source_table_name, 3);
+
+    -- Get the column names from the source table
+    DECLARE @source_query NVARCHAR(MAX);
+    SET @source_query = 'INSERT INTO #temp_columns (column_name) SELECT COLUMN_NAME FROM ' + @source_db + '.INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ''' + PARSENAME(@source_table_name, 1) + '''';
+    EXEC(@source_query);
+
+	SET @source_query = 'INSERT INTO #temp_columns2 (column_name) SELECT COLUMN_NAME FROM ' + @source_db + '.INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ''' + PARSENAME(@source_table_name, 1) + '''';
+    EXEC(@source_query);
+
+    -- Debug: Print the column names from source table
+    PRINT 'Source Table Columns:';
+    SELECT column_name FROM #temp_columns;
+    
+    -- Remove column names that exist in the destination table
+    DECLARE @destination_db NVARCHAR(255);
+    DECLARE @destination_table NVARCHAR(255);
+    SET @destination_db = PARSENAME(@destination_table_name, 3);
+    SET @destination_table = PARSENAME(@destination_table_name, 1);
+    
+    DECLARE @sql NVARCHAR(MAX);
+    SET @sql = 'DELETE FROM #temp_columns WHERE column_name IN (SELECT COLUMN_NAME FROM ' + @destination_db + '.INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ''' + @destination_table + ''')';
+    EXEC(@sql);
+    
+    -- Debug: Print the remaining columns
+    PRINT 'Remaining Columns:';
+    SELECT column_name FROM #temp_columns;
+
+    SET @sql = 'DELETE FROM #temp_columns2 WHERE column_name IN (SELECT COLUMN_NAME FROM #temp_columns)';
+    EXEC(@sql);
+
+    -- Concatenate column names into @ColumnList
+    -- Concatenate column names into @ColumnList
+	SET @ColumnList = '';
+	SELECT @ColumnList = @ColumnList + ', ' + column_name
+	FROM #temp_columns2;
+
+	-- Remove leading comma and space
+	IF LEN(@ColumnList) > 2
+		SET @ColumnList = SUBSTRING(@ColumnList, 3, LEN(@ColumnList) - 2);
+
+    -- Drop the temporary tables
+    DROP TABLE #temp_columns;
+    DROP TABLE #temp_columns2;
+END;
+
+
+
+DECLARE @result nvarchar(max);
+
+EXEC Find_Extra_Columns_8 
+    'TestDB_Backup.dbo.ChildTable1', 
+    'TestDB.dbo.ChildTable1', 
+    @ColumnList = @result OUTPUT;
+
+PRINT CONVERT(VARCHAR, @result);
+
+
+
+DECLARE @result nvarchar(max);
+
+EXEC Find_Extra_Columns_Modified_2
+    'TestDB_Backup.dbo.ChildTable1', 
+    'TestDB.dbo.ChildTable1', 
+    @ColumnList = @result OUTPUT;
+
+PRINT CONVERT(VARCHAR, @result);
+
+
+use TestDB;
+
+
+DECLARE @ColumnList NVARCHAR(MAX);
+SELECT @ColumnList = COALESCE(@ColumnList + ', ', '') + QUOTENAME(column_name)
+FROM temp_columns2;
+
+
+
+EXEC sp_executesql @SQL, N'@LogsID INT', @LogsID;
+
+
+
+
+
+
+
+
+
+
+CREATE PROCEDURE UPDATION_OF_DATA_FINAL_104
+    @SourceDatabase NVARCHAR(100),
+    @SourceSchema NVARCHAR(100),
+    @SourceTable NVARCHAR(100),
+    @TargetDatabase NVARCHAR(100),
+    @TargetSchema NVARCHAR(100),
+    @TargetTable NVARCHAR(100),
+    @condition NVARCHAR(MAX),
+    @LogsID INT OUTPUT,
+    @message NVARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @SQL NVARCHAR(MAX);
+    DECLARE @ChildTableName NVARCHAR(100);
+
+    -- Create temporary table to store child table names
+    CREATE TABLE #ChildTables (TableName NVARCHAR(100));
+
+    -- Find child tables
+    INSERT INTO #ChildTables
+    SELECT t.name
+    FROM sys.foreign_keys AS fk
+    INNER JOIN sys.tables AS t ON fk.parent_object_id = t.object_id
+    WHERE OBJECT_NAME(fk.referenced_object_id) = @SourceTable
+      AND SCHEMA_NAME(t.schema_id) = @SourceSchema
+      AND DB_NAME() = @SourceDatabase;
+
+    -- Create Log_Table if it doesn't exist
+    SET @SQL = '
+        IF NOT EXISTS (SELECT 1 FROM ' + QUOTENAME(@TargetDatabase) + '.' + QUOTENAME(@TargetSchema) + '.Log_Table)
+        BEGIN
+            CREATE TABLE ' + QUOTENAME(@TargetDatabase) + '.' + QUOTENAME(@TargetSchema) + '.Log_Table (
+                LogsID INT IDENTITY(1,1) PRIMARY KEY,
+                LogMessage NVARCHAR(MAX),
+                CreationDateTime DATETIME DEFAULT GETDATE()
+            );
+        END;
+    ';
+    EXEC(@SQL);
+
+    -- Insert a record into Log_Table and get the generated LogsID
+    SET @SQL = '
+        USE ' + QUOTENAME(@TargetDatabase) + ';
+        INSERT INTO ' + QUOTENAME(@TargetSchema) + '.Log_Table (LogMessage)
+        VALUES (@message);
+        SET @LogsID = SCOPE_IDENTITY();';
+    EXEC sp_executesql @SQL, N'@message NVARCHAR(MAX), @LogsID INT OUTPUT', @message, @LogsID OUTPUT;
+
+    -- Check if target table exists, if yes, insert data
+    SET @SQL = '
+        IF EXISTS (SELECT 1 FROM ' + QUOTENAME(@TargetDatabase) + '.INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ' + QUOTENAME(@TargetSchema, '''') + ' AND TABLE_NAME = ''' + @TargetTable + ''')
+        BEGIN
+            USE ' + QUOTENAME(@TargetDatabase) + ';
+            INSERT INTO ' + QUOTENAME(@TargetSchema) + '.' + QUOTENAME(@TargetTable) + '
+            SELECT @LogsID AS LogsID, *
+            FROM ' + QUOTENAME(@SourceDatabase) + '.' + QUOTENAME(@SourceSchema) + '.' + QUOTENAME(@SourceTable) +
+            @condition + ';
+        END;
+    ';
+    -- Pass @LogsID as parameter
+    EXEC sp_executesql @SQL, N'@LogsID INT', @LogsID;
+
+
+			 -- Check if child table exists, if yes, Insert it
+		DECLARE childTablesCursor CURSOR FOR
+		SELECT TableName FROM #ChildTables;
+		OPEN childTablesCursor;
+		FETCH NEXT FROM childTablesCursor INTO @ChildTableName;
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+		
+		BEGIN TRY
+
+
+			SET @SQL = '
+				IF EXISTS (SELECT 1 FROM ' + QUOTENAME(@TargetDatabase) + '.INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ' + QUOTENAME(@TargetSchema, '''') + ' AND TABLE_NAME = ''' + @ChildTableName + ''')
+				BEGIN
+					USE ' + QUOTENAME(@TargetDatabase) + ';
+					INSERT INTO ' + QUOTENAME(@TargetSchema) + '.' + QUOTENAME(@ChildTableName) + '
+					SELECT @LogsID AS LogsID, *
+					FROM ' + QUOTENAME(@SourceDatabase) + '.' + QUOTENAME(@SourceSchema) + '.' + QUOTENAME(@ChildTableName) + @condition + ';
+				END;
+			';
+			EXEC sp_executesql @SQL, N'@LogsID INT', @LogsID;
+
+			END TRY
+			BEGIN CATCH
+
+				BEGIN TRY
+					 SET @message = 'Altering the Table Structure';
+					 PRINT @message;
+					 DECLARE @TargetTablePath NVARCHAR(1000);
+					 SET @TargetTablePath = QUOTENAME(@TargetDatabase) + '.' + QUOTENAME(@TargetSchema) + '.' + QUOTENAME(@ChildTableName);
+				 
+					 PRINT @ChildTableName;
+					 EXEC FindMissingColumns_with_datatype_and_update_1 @ChildTableName, @TargetTablePath;
+
+					SET @SQL = '
+					IF EXISTS (SELECT 1 FROM ' + QUOTENAME(@TargetDatabase) + '.INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ' + QUOTENAME(@TargetSchema, '''') + ' AND TABLE_NAME = ''' + @ChildTableName + ''')
+					BEGIN
+						USE ' + QUOTENAME(@TargetDatabase) + ';
+						INSERT INTO ' + QUOTENAME(@TargetSchema) + '.' + QUOTENAME(@ChildTableName) + '
+						SELECT @LogsID AS LogsID, *
+						FROM ' + QUOTENAME(@SourceDatabase) + '.' + QUOTENAME(@SourceSchema) + '.' + QUOTENAME(@ChildTableName) + @condition + ';
+					END;
+				';
+				EXEC sp_executesql @SQL, N'@LogsID INT', @LogsID;
+
+				END TRY
+				BEGIN CATCH
+
+				DECLARE @TargetTablePath_Dest NVARCHAR(1000);
+				DECLARE @TargetTablePath_Source NVARCHAR(1000);
+				SET @TargetTablePath_Dest = QUOTENAME(@TargetDatabase) + '.' + QUOTENAME(@TargetSchema) + '.' + QUOTENAME(@ChildTableName);
+				SET @TargetTablePath_Source = QUOTENAME(@SourceDatabase) + '.' + QUOTENAME(@SourceSchema) + '.' + QUOTENAME(@ChildTableName);
+
+				DECLARE @result nvarchar(max);
+				EXEC Find_Extra_Columns_Modified_2 @TargetTablePath_Dest, @TargetTablePath_Source, @ColumnList = @result OUTPUT;
+				PRINT CONVERT(VARCHAR, @result);
+
+				
+
+				SET @SQL = '
+				IF EXISTS (SELECT 1 FROM ' + QUOTENAME(@TargetDatabase) + '.INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ' + QUOTENAME(@TargetSchema, '''') + ' AND TABLE_NAME = ''' + @ChildTableName + ''')
+				BEGIN
+					USE ' + QUOTENAME(@TargetDatabase) + ';
+					INSERT INTO ' + QUOTENAME(@TargetSchema) + '.' + QUOTENAME(@ChildTableName) + ' ('+'LogsID, '+ @result + ')
+					SELECT @LogsID AS LogsID, ' + @result + '
+					FROM ' + QUOTENAME(@SourceDatabase) + '.' + QUOTENAME(@SourceSchema) + '.' + QUOTENAME(@ChildTableName) + @condition + ';
+				END;
+				';
+
+				PRINT 'Dynamic SQL Statement:';
+				PRINT @SQL;
+
+				EXEC sp_executesql @SQL, N'@LogsID INT', @LogsID;
+
+				-- Write the part here
+				END CATCH
+
+			END CATCH
+
+			FETCH NEXT FROM childTablesCursor INTO @ChildTableName;
+
+		END;
+
+		CLOSE childTablesCursor;
+		DEALLOCATE childTablesCursor;
+
+    -- Clean up temporary table
+    DROP TABLE #ChildTables;
+END;
+
+exec UPDATION_OF_DATA_FINAL_104 'TestDB', 'dbo', 'ParentTable', 'TestDB_Backup', 'dbo', 'ParentTable', '', '','Copying';
+
+
